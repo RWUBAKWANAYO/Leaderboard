737df8d6ea0846b2bae4f1f69c822924a264cadc
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-05-11 22:15:25 +0200

    fetch data from API

--- src/score.js
+++ src/score.js
@@ -1,46 +1,26 @@
 class Score {
-  constructor(id, name, score) {
-    this.id = id;
-    this.name = name;
+  constructor(user, score) {
+    this.user = user;
     this.score = score;
   }
 
-  scoreData = [
-    {
-      id: 1,
-      name: 'john',
-      score: 10,
-    },
-    {
-      id: 2,
-      name: 'Doe',
-      score: 10,
-    },
-    {
-      id: 3,
-      name: 'will',
-      score: 10,
-    },
-    {
-      id: 3,
-      name: 'smith',
-      score: 10,
-    },
-  ]
+  scoreData = [];
+
+  baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/:id/scores/';
 
   getScore = () => {
     const recentScore = document.querySelector('.recent-score-group');
-    recentScore.innerHTML = this.scoreData.map((element, index) => `<li class=${index % 2 !== 0 ? 'list-gray' : 'list-white'}>${element.name} : ${element.score}</li>`).join('');
+    recentScore.innerHTML = this.scoreData.map((element, index) => `<li class=${index % 2 !== 0 ? 'list-gray' : 'list-white'}>${element.user} : ${element.score}</li>`).join('');
   }
 
-  addScore=({ name, scoreNum }) => {
-    this.scoreData.push({
-      id: this.scoreData.length + 1,
-      name,
-      score: scoreNum,
-    });
-    this.getScore();
-  }
+  fetchScore = async () => {
+    try {
+      const data = await fetch(this.baseUrl);
+      const res = await data.json();
+      res.result.map((item) => this.scoreData.push(item));
+      return this.getScore();
+    } catch (err) { return err; }
+  };
 }
 
 export default Score;
