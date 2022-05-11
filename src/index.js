18ce1b27d4dc7b6606d2177edc277dd2963dd5a4
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-05-12 01:13:16 +0200

    Fix logic error

--- src/index.js
+++ src/index.js
@@ -14,4 +14,4 @@ addNewScore.addEventListener('submit', (e) => {
 
 const refreshButton = document.querySelector('.refresh-button');
 refreshButton.addEventListener('click', newScore.fetchScore);
-window.addEventListener('load', newScore.getScore);
+window.addEventListener('load', newScore.fetchScore);
